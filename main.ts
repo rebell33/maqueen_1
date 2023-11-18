d6d850a0df011aa783e0a6cca217d611071da189
rebell33 <151248913+rebell33@users.noreply.github.com>
2023-11-18 17:28:03 +0100

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,11 @@
-basic.forever(function () {
-    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
-    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
-    basic.pause(1000)
-    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
-    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
-    basic.pause(1000)
-    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
-    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
-    basic.pause(1000)
-    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 255)
-    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 255)
-    basic.pause(1000)
-    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 255)
-    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 0)
-    basic.pause(1000)
-})
+// Maqueen Motoren
+let M1_IN1 = DigitalPin.P0
+let M1_IN2 = DigitalPin.P1
+let M2_IN1 = DigitalPin.P8
+let M2_IN2 = DigitalPin.P12
+
+// Funktion zum Steuern der Maqueen-Motoren
+function maqueenDrive(leftSpeed: number, rightSpeed: number): void {
+    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, leftSpeed)
+    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, rightSpeed)
+}
