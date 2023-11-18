93c2c1c11b1751e2532b09a0196e50c9e85bc3e0
rebell33 <151248913+rebell33@users.noreply.github.com>
2023-11-18 17:29:36 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-// Maqueen Motoren
+// Funktion zum Steuern der Maqueen-Motoren
+function maqueenDrive (leftSpeed: number, rightSpeed: number) {
+    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, leftSpeed)
+    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, rightSpeed)
+}
 let M1_IN1 = DigitalPin.P0
 let M1_IN2 = DigitalPin.P1
 let M2_IN1 = DigitalPin.P8
 let M2_IN2 = DigitalPin.P12
-
-// Funktion zum Steuern der Maqueen-Motoren
-function maqueenDrive(leftSpeed: number, rightSpeed: number): void {
-    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, leftSpeed)
-    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, rightSpeed)
-}
